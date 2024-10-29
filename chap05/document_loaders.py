4d739a5c1760ba6d1830928ecdbc8cb9a7c29878
seon54 <seonhyeongpark54@gmail.com>
2024-10-29 14:13:56 +0900

    add Document Transformer

--- chap05/document_loaders.py
+++ chap05/document_loaders.py
@@ -1,15 +1,17 @@
 from dotenv import dotenv_values
 from langchain_community.document_loaders import GitLoader
+from langchain_text_splitters import CharacterTextSplitter
 
 
 def file_filter(file_path):
-    return file_path.endswith(".mdx")
+    return file_path.endswith(".py")
 
 
 if __name__ == '__main__':
     config = dotenv_values("../.env")
     api_key = config["OPENAI_API_KEY"]
 
+    # document loader
     loader = GitLoader(
         clone_url="https://github.com/langchain-ai/langchain",
         repo_path=". /langchain",
@@ -19,3 +21,9 @@ if __name__ == '__main__':
 
     raw_docs = loader.load()
     print(len(raw_docs))
+
+    # document transformer
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    docs = text_splitter.split_documents(raw_docs)
+
+    print(len(docs))
