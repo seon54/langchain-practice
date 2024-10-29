9758aef0361857e07a56bca413058f7860b74152
seon54 <seonhyeongpark54@gmail.com>
2024-10-29 16:12:24 +0900

    add Retrievers

--- chap05/document_loaders.py
+++ chap05/document_loaders.py
@@ -1,10 +1,12 @@
 from dotenv import dotenv_values
 from langchain_community.document_loaders import GitLoader
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAIEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 
 
 def file_filter(file_path):
-    return file_path.endswith(".py")
+    return file_path.endswith(".mdx")
 
 
 if __name__ == '__main__':
@@ -27,3 +29,18 @@ if __name__ == '__main__':
     docs = text_splitter.split_documents(raw_docs)
 
     print(len(docs))
+
+    embeddings = OpenAIEmbeddings(api_key=api_key)
+
+    query = "AWS S3에서 데이터를 불러올 수 있는 DocumentLoader가 있나요?"
+
+    db = Chroma.from_documents(docs, embeddings)
+
+    retriever = db.as_retriever()
+
+    context_docs = retriever.get_relevant_documents(query)
+    print(f'len = {len(context_docs)}')
+
+    first_doc = context_docs[0]
+    print(f'metadata = {first_doc.metadata}')
+    print(first_doc.page_content)
